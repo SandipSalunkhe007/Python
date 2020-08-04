ffeecd9f235c707ebfb2adba62e26f74422e7bf8
Sandip Salunkhe <55046838+SandipSalunkhe007@users.noreply.github.com>
2020-08-04 14:28:34 +0530

    Update tutorial41.py

--- Tutorials/tutorial41.py
+++ Tutorials/tutorial41.py
@@ -1,5 +1,6 @@
 # Operator overloading and Dunder/Magic method
-# Dunder or magic methods in Python are the methods having two prefix and suffix underscores in the method name. Dunder here means “Double Under (Underscores)”. These are commonly used for operator overloading. Few examples for magic methods are: __init__, __add__, __len__, __repr__ etc.
+# Dunder or magic methods in Python are the methods having two prefix and suffix underscores in the method name. 
+# Dunder here means “Double Under (Underscores)”. These are commonly used for operator overloading. Few examples for magic methods are: __init__, __add__, __len__, __repr__ etc.
 
 class Employee:
     no_of_leave = 9
